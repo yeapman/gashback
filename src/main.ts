a60e6e5adee950925fef16c3a38d9905d00dac78
Yaroslav <ukrpanama@gmail.com>
2025-04-17 02:23:20 +0200

    make it better2

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
-
-  await app.listen(configService.getOrThrow('app.port', { infer: true }));
+  const port = process.env.PORT || configService.getOrThrow('app.port', { infer: true });
+  await app.listen(port);
+  // await app.listen(configService.getOrThrow('app.port', { infer: true }));
 }
 void bootstrap();
